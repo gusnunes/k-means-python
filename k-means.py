1b1f8610fd4dd57b22a68348d18ea51d39a90cf9
gusnunes <daniloaugusto_nunes@hotmail.com>
2020-06-03 01:00:36 -0300

    Concluída a funcionalidade de leitura de arquivo

--- k-means.py
+++ k-means.py
@@ -59,24 +59,19 @@ def k_means(data, k):
     return groups
 
 def main():
-    # por enquanto os dados sao constantes
-    # para fins de teste
-    data = [(1.0,1.0), (1.5,2.0), (3.0,4.0), (5.0,7.0),
-            (3.5,5.0), (4.5,5.0), (3.5,4.5)]
-    k = 2
-
-    groups = []
+    k = 5
+    data = []
 
     f = open("data_ex1.txt", "r")
     for line in f:
         line = line.strip("()\n")   # remover os parenteses e quebras de linha da leitura
         
         aux = line.split(",")
-        groups.append((float(aux[0]), float(aux[1])))   # transformar as coordenas de str para float
+        data.append((float(aux[0]), float(aux[1])))   # transformar as coordenas de str para float
         
     groups = k_means(data, k)
 
     for i in range(k):
-        print(f"Cluster {i+1}:",groups[i])
+        print(f"\nCluster {i+1}: \n{groups[i]}\n")
     
 main()
