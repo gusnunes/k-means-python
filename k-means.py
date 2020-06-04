67d26cc4eda62756b9ee90da287913531798f498
gusnunes <daniloaugusto_nunes@hotmail.com>
2020-06-03 22:06:32 -0300

    Criando funcao que lê arquivo

--- k-means.py
+++ k-means.py
@@ -1,5 +1,18 @@
 import random
 
+def read_file(file_name):
+    data = []
+
+    f = open(file_name, "r")
+    for line in f:
+        line = line.strip("()\n")   # remover os parenteses e quebras de linha da leitura
+
+        aux = line.split(",")
+        data.append((float(aux[0]), float(aux[1])))   # transformar as coordenas de str para float
+    
+    f.close()
+    return data
+
 def inicial_centroids(lista, k):   # sortear itens que serao os centroides dos grupos
     centroids = []
     for i in range(k):
@@ -16,12 +29,12 @@ def calc_distancia(p1, p2):   # ponto p1 e ponto p2
     return (((x2-x1)**2) + ((y2-y1)**2))**0.5
 
 def calc_centroid(group):
-    sum_x = sum_y = 0
-    size  = len(group)
+    sum_x = sum_y = size = 0
 
     for x,y in group:
         sum_x += x
         sum_y += y
+        size  += 1
 
     return (sum_x/size, sum_y/size)
 
@@ -50,24 +63,18 @@ def k_means(data, k):
             
             groups[index].append(coord)  # insere coordenada no seu devido grupo
             centroids[index] = calc_centroid(groups[index])   # atualiza o centroide do grupo que recebeu nova coordenada
-
+        
         if past_centroids == centroids:   # verificar se houve mudanças nos centroides
             relocations = False
         else:
             past_centroids = centroids[:]
         
+    #print(f"Centroides finais: {centroids}\n")
     return groups
 
 def main():
-    k = 5
-    data = []
-
-    f = open("data_ex1.txt", "r")
-    for line in f:
-        line = line.strip("()\n")   # remover os parenteses e quebras de linha da leitura
-        
-        aux = line.split(",")
-        data.append((float(aux[0]), float(aux[1])))   # transformar as coordenas de str para float
+    k = 3   # quantidade de clusters
+    data = read_file("data_ex2.txt")   # nome do arquivo como argumento
         
     groups = k_means(data, k)
 
